79259d5ea18a7590889b07d6aab62a72843a820b
okaheel <omka7201@colorado.edu>
2020-02-05 16:16:33 -0700

    Completed with 3 addtiional incorrect results for the phone number search

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# Authors : Your Name & Your partner's name
+# Authors : Omar Kaheel 
 # Date: 1/1/2019
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
+
+#prompt user to enter a filename and read a regex
+#read user input
+#use grep to search the file to find that regex
+echo "enter a filename: "
+read filename
+echo "enter a regex to search"
+read regex
+
+grep $regex $filename
+
+echo "number of phone numbers:"
+grep '\([0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' $filename 
+grep '\(([0-9]\{3\})\|[0-9]\{3\}\)[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' $filename -c
